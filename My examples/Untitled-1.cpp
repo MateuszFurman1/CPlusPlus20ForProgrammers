afbf8f0580c73977c6952e1c46ddf5115f9d6ae2
MateuszFurman1 <106817902+MateuszFurman1@users.noreply.github.com>
2023-10-19 11:09:47 +0200

    c++ first step

---
+++ My examples/Untitled-1.cpp
@@ -0,0 +1,10 @@
+// fig03_03.cpp
+// Analysis of examination results using nested control statements.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number = 1;
+    int result = number++;
+    cout << result;
+}
